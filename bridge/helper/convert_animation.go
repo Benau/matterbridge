4e70092f2058398e49fab8949d6857972339ef0d
Benau <Benau@users.noreply.github.com>
2024-03-08 09:36:02 +0800

    .....

--- bridge/helper/convert_animation.go
+++ bridge/helper/convert_animation.go
@@ -2,11 +2,12 @@
 
 package helper
 
-// #cgo LDFLAGS: -lavformat -lavcodec -lavutil -lswscale -lgifski -O0 -g
+// #cgo LDFLAGS: -lavformat -lavcodec -lavutil -lswscale -lgifski
 // #include <animation_to_gif.h>
 // #include <string.h>
 // int writer_cgo(size_t buffer_length, const uint8_t *buffer, void *user_data);
 import "C"
+import "strings"
 import "unsafe"
 //import "io/ioutil"
 
@@ -17,7 +18,8 @@ func writerGo(size C.size_t, buffer unsafe.Pointer, user_data unsafe.Pointer) in
 	if size == 0 {
 		return 0
 	}
-	output = append(output, (*[1 << 30]byte)(buffer)[:size]...)
+	data := C.GoBytes(buffer, C.int(size))
+	output = append(output, data...)
 	return 0
 }
 
@@ -26,7 +28,13 @@ func ConvertAnimation(name *string, data *[]byte) {
 	var ptr *C.uint8_t = (*C.uint8_t)(unsafe.Pointer(&(*data)[0]));
 	if C.animationToGif(ptr, C.int(len(*data)), (C.writeCallback)(unsafe.Pointer(C.writer_cgo))) == 0 {
 		*data = output
-		*name = *name + ".gif"
+		parts := strings.Split(*name, ".")
+		// Extract the first part
+		if len(parts) > 0 {
+			*name = parts[0] + ".gif"
+		} else {
+			*name = *name + ".gif"
+		}
 		//ioutil.WriteFile(*name, *data, 0644)
 	}
 }
